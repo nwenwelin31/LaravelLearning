4b4ad639b5e474d17d5e1a8ab3d16a78b5514d63
Nwe Nwe Lin <nwenwelinn@gicjp.com>
2024-03-06 16:56:13 +0630

    axios

--- vite-project/src/components/GuestLayout.jsx
+++ vite-project/src/components/GuestLayout.jsx
@@ -1,15 +1,18 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useStateContext } from "../contexts/ContextProvider"
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function GuestLayout() {
-  const {token} = useStateContext();
+  const { token } = useStateContext();
   // debugger;
-  if(token){
-    return <Navigate to="/users" />
+  if (token) {
+    return <Navigate to="/users" />;
   }
   return (
-    <div>
-      <Outlet/>
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+
+        <Outlet />
+      </div>
     </div>
-  )
+  );
 }
--- vite-project/src/views/Signup.jsx
+++ vite-project/src/views/Signup.jsx
@@ -3,24 +3,18 @@ import { Link } from "react-router-dom";
 export default function Signup() {
   const onSubmit = (ev) => {
     ev.preventDefault();
-}
+  };
   return (
-    <div className="login-signup-form animated fadeInDown">
-        <div className="form">
-            <from onSubmit={onSubmit}>
-            <h1 className="title">
-                Sign up for free
-            </h1>
-                <input placeholder="Full Name" type="text"/>
-                <input placeholder="Email Address" type="email"/>
-                <input type="password" placeholder="Password"/>
-                <input type="password" placeholder="Confirm Password"/>
-                <button className="btn btn-block">Login</button>
-                <p className="message">
-                    Already Registered? <Link to="/login">Sign in</Link>
-                </p>
-            </from>
-        </div>
-    </div>
-  )
+    <from onSubmit={onSubmit}>
+      <h1 className="title">Sign up for free</h1>
+      <input placeholder="Full Name" type="text" />
+      <input placeholder="Email Address" type="email" />
+      <input type="password" placeholder="Password" />
+      <input type="password" placeholder="Confirm Password" />
+      <button className="btn btn-block">Login</button>
+      <p className="message">
+        Already Registered? <Link to="/login">Sign in</Link>
+      </p>
+    </from>
+  );
 }
